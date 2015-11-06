2f9023dcbf0c930272c79ef23c468ad31ea03b9e
Meninblack007 <sanyam.53jain@gmail.com>
2015-11-06 05:55:47 -0500

    tomato: Disable Dexpreopt
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,17 +45,6 @@ TARGET_TRANSPARENT_COMPRESSION_METHOD := lz4
 # CPU
 TARGET_CPU_CORTEX_A53 := true
 
-# Dexopt, only if we can fit that in
-ifneq ($(TARGET_TRANSPARENT_COMPRESSION_METHOD),)
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-endif
-
 # GPS
 TARGET_GPS_HAL_PATH := $(DEVICE_PATH)/gps
 TARGET_NO_RPC := true
